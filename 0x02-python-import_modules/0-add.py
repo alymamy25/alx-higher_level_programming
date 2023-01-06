3ea15e03eee725abdebc2ddd30e39e77d224bc98
Alymamy <alymamygbane25@gmail.com>
2023-01-06 14:37:08 +0000

    pas

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
+from add_0 import add
 
 
-def add(a, b):
-"""My addition function
 
-Args:
-a: first integer
-b: second integer
+if __name__ == "__main__":
 
-Returns:
-The return value. a + b
-"""
-return (a + b)
+    a = 1
+
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
